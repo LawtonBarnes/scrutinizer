669c72eab05f9eda0fdcefbf911717e95e8aff0f
Lawton Barnes <lawtonbarnes@gmail.com>
2026-08-23 15:51:14 -0500

    Bump VERSION to 2.8 -- session wrap-up

--- scrutinizer.py
+++ scrutinizer.py
@@ -59,7 +59,7 @@ os.environ.setdefault("SDL_VIDEODRIVER", "dummy")
 
 import pygame  # noqa: E402  (must come after SDL env vars are set)
 
-VERSION = "2.7"
+VERSION = "2.8"
 
 BASE_DIR = Path(__file__).resolve().parent
 FONT_PATH = BASE_DIR / "VCR_OSD_MONO_1.001.ttf"
